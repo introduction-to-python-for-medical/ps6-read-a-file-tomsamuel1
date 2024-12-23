427c67b7e1cb43c2701dd7c8435f04c39ff1c037
tomsamuel1 <tomstoms9@gmail.com>
2024-12-23 19:25:21 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
+    codon2aa = {}
+    for row in rows:
+      cells = row.strip().split('\t')
+      codon = cells[0]
+      aa = cells[2]
+      codon2aa[codon] = aa
+    return codon2aa
 
